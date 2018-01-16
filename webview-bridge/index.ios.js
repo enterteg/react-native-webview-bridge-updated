9a91e9bb8e82f51aef855d7db2c8219305f487e8
Dawid Jagieła <dawid0000@gmail.com>
2018-01-16 10:46:09 +0100

    createClass to class

--- webview-bridge/index.ios.js
+++ webview-bridge/index.ios.js
@@ -90,13 +90,8 @@ var defaultRenderError = (errorDomain, errorCode, errorDesc) => (
 /**
  * Renders a native WebView.
  */
-var WebViewBridge = React.createClass({
-  statics: {
-    JSNavigationScheme: JSNavigationScheme,
-    NavigationType: NavigationType,
-  },
-
-  propTypes: {
+class WebViewBridge extends React.Component {
+  propTypes = {
     /**
      * Will be called once the message is being sent from webview
      */
@@ -107,23 +102,23 @@ var WebViewBridge = React.createClass({
     keyboardDisplayRequiresUserAction: PropTypes.bool,
 
     turnOffScrolling: PropTypes.bool,
-  },
+  }
 
-  getInitialState: function() {
+  getInitialState = () => {
     return {
       viewState: WebViewBridgeState.IDLE,
       lastErrorEvent: (null: ?ErrorEvent),
       startInLoadingState: true,
     };
-  },
+  }
 
-  componentWillMount: function() {
+  componentWillMount = () => {
     if (this.props.startInLoadingState) {
       this.setState({viewState: WebViewBridgeState.LOADING});
     }
-  },
+  }
 
-  render: function() {
+  render() {
     var otherView = null;
 
     if (this.state.viewState === WebViewBridgeState.LOADING) {
@@ -202,57 +197,57 @@ var WebViewBridge = React.createClass({
         {otherView}
       </View>
     );
-  },
+  }
 
-  goForward: function() {
+  goForward = () => {
     UIManager.dispatchViewManagerCommand(
       this.getWebViewBridgeHandle(),
       UIManager.RCTWebViewBridge.Commands.goForward,
       null
     );
-  },
+  }
 
-  goBack: function() {
+  goBack = () => {
     UIManager.dispatchViewManagerCommand(
       this.getWebViewBridgeHandle(),
       UIManager.RCTWebViewBridge.Commands.goBack,
       null
     );
-  },
+  }
 
-  reload: function() {
+  reload = () => {
     UIManager.dispatchViewManagerCommand(
       this.getWebViewBridgeHandle(),
       UIManager.RCTWebViewBridge.Commands.reload,
       null
     );
-  },
+  }
 
-  sendToBridge: function (message: string) {
+  sendToBridge = (message: string) => {
     WebViewBridgeManager.sendToBridge(this.getWebViewBridgeHandle(), message);
-  },
+  }
 
   /**
    * We return an event with a bunch of fields including:
    *  url, title, loading, canGoBack, canGoForward
    */
-  updateNavigationState: function(event: Event) {
+  updateNavigationState = (event: Event) => {
     if (this.props.onNavigationStateChange) {
       this.props.onNavigationStateChange(event.nativeEvent);
     }
-  },
+  }
 
-  getWebViewBridgeHandle: function(): any {
+  getWebViewBridgeHandle = () => {
     return ReactNative.findNodeHandle(this.refs[RCT_WEBVIEWBRIDGE_REF]);
-  },
+  }
 
-  onLoadingStart: function(event: Event) {
+  onLoadingStart = (event: Event) => {
     var onLoadStart = this.props.onLoadStart;
     onLoadStart && onLoadStart(event);
     this.updateNavigationState(event);
-  },
+  }
 
-  onLoadingError: function(event: Event) {
+  onLoadingError = (event: Event) => {
     event.persist(); // persist this event because we need to store it
     var {onError, onLoadEnd} = this.props;
     onError && onError(event);
@@ -263,9 +258,9 @@ var WebViewBridge = React.createClass({
       lastErrorEvent: event.nativeEvent,
       viewState: WebViewBridgeState.ERROR
     });
-  },
+  }
 
-  onLoadingFinish: function(event: Event) {
+  onLoadingFinish = (event: Event) => {
     var {onLoad, onLoadEnd} = this.props;
     onLoad && onLoad(event);
     onLoadEnd && onLoadEnd(event);
@@ -273,7 +268,7 @@ var WebViewBridge = React.createClass({
       viewState: WebViewBridgeState.IDLE,
     });
     this.updateNavigationState(event);
-  },
+  }
 });
 
 var RCTWebViewBridge = requireNativeComponent('RCTWebViewBridge', WebViewBridge, {
@@ -281,7 +276,7 @@ var RCTWebViewBridge = requireNativeComponent('RCTWebViewBridge', WebViewBridge,
     onLoadingStart: true,
     onLoadingError: true,
     onLoadingFinish: true,
-  },
+  }
 });
 
 var styles = StyleSheet.create({
@@ -316,7 +311,7 @@ var styles = StyleSheet.create({
   },
   webView: {
     backgroundColor: '#ffffff',
-  }
+  },
 });
 
 module.exports = WebViewBridge;
